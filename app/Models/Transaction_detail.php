6dd5234616e9e02d1de8dcb29477da64d4446997
hendragff <hdrrr21@gmail.com>
2023-01-11 13:17:33 +0700

    nambahin category sama cart

--- app/Models/Transaction_detail.php
+++ app/Models/Transaction_detail.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class transaction_detail extends Model
 {
     use HasFactory;
-    protected $fillable = [];
-    protected $table = '';
+    protected $fillable = ["id", "transacation_id", "item_id", "qtt", "subtotal"];
+    protected $table = 'transaction_detail';
 }
